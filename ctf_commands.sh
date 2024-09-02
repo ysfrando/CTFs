8210edc7cd2a60e0332604614bfbb07bde286c57
ysfrandom <yusufmunir.sec@gmail.com>
2024-09-02 07:01:57 -0400

    Create ctf_commands.sh

---
+++ ctf_commands.sh
@@ -0,0 +1,32 @@
+# List of commands i've used in CTFs
+# -----------
+# Enumeration
+# -----------
+
+# Network Enum
+sudo nmap -T4 --min-rate 1000 -sV -sC -p- -oN nmap_report <ip>
+
+# Subdomain Enum
+gobuster dns -d target.com -w /path/to/wordlist.txt
+
+
+# Server Enum
+gobuster dir -u <url> -w <wordlist>
+nikto -h <url>
+nikto -h http://target.url -useragent "Mozilla/5.0" # Change user agent
+
+
+# ------------
+# Exploitation
+# ------------
+
+# Brute Force
+hydra -l <username> -P <password_list> ssh://<target IP>
+python3 sqlmap.py -r /home/tryhackme/expose/expose.req — dump
+
+
+
+
+
+
+
